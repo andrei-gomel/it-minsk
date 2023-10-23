371e25e6150d4db3fe5508b697981c1d1878c760
Андрей <84657319+andrei-gomel@users.noreply.github.com>
2023-10-23 10:27:54 +0300

    Update App.php

--- src/core/App.php
+++ src/core/App.php
@@ -2,24 +2,3 @@
 
 namespace vendor\core;
 
-//use vendor\core\Registry;
-use vendor\core\ErrorHandler;
-//use vendor\libs\rb;
-
-class App
-{
-    public static $app;
-
-    public function __construct()
-    {
-        //self::$app = Registry::instance();
-
-        new ErrorHandler;
-
-        /*require_once LIBS . '/rb.php';
-
-		\R::setup('mysql:host=localhost;dbname=video_consult;charset=utf8', 'root', '');
-
-		\R::freeze(true);*/
-    }
-}
