e4e973fc1e0ebf9e306c03231db0138cb61f061b
Андрей <84657319+andrei-gomel@users.noreply.github.com>
2023-10-23 10:28:57 +0300

    Update View.php

--- src/core/base/View.php
+++ src/core/base/View.php
@@ -17,22 +17,14 @@ class View
         } else {
             $this->layout = $layout ?: LAYOUT;
         }
-
-        //dd($this->layout,0);
-        //dd($this->view);
-
     }
 
     public function render(array|string $vars): void
     {
-        //dd($vars,0);
-
         if (is_array($vars))
             extract($vars);
 
         $file_view = APP . "/Views/" . $this->route['controller'] . "/{$this->view}.php";
-        //dd($file_view);
-        //ob_start();
 
         if (is_file($file_view)) 
         {
@@ -42,19 +34,5 @@ class View
         {
             echo '<p>Не найден вид <b>' . $this->view . '</b></p>';
         }
-
-        //$content = ob_get_clean();
-
-        /*
-        if (false !== $this->layout) {
-            $file_layout = APP . "/views/layouts/{$this->layout}.php";
-
-            if (is_file($file_layout)) {
-                require $file_layout;
-            } else {
-                echo '<p>Не найден шаблон <b>' . $this->layout . '</b></p>';
-            }
-        }
-        */
     }
 }
